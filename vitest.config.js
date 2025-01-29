18d3b4fb3c6b494295196192de6a246bb5db3942
Hugo C <911307+hugocaillard@users.noreply.github.com>
2025-01-29 17:36:03 +0100

    chore: clarinet-sdk beta9 (#1)

--- vitest.config.js
+++ vitest.config.js
@@ -35,7 +35,6 @@ export default defineConfig({
     environmentOptions: {
       clarinet: {
         ...getClarinetVitestsArgv(),
-        initBeforeEach: false,
         // add or override options
       },
     },
